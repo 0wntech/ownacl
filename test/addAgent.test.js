59e7a71ea627218d60d684abfc7e4323b767e7fb
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-08-09 20:06:51 +0200

    Add auto-creation of access files when adding agents

--- test/addAgent.test.js
+++ test/addAgent.test.js
@@ -1,12 +1,12 @@
 const { expect } = require("chai");
 const auth = require("solid-auth-cli");
 const rdf = require("rdflib");
-const ns = require("solid-namespace")(rdf);
-const fs = require("fs");
+const PodClient = require("ownfiles");
 const aclClient = require("../index");
 
 const resource = "https://lalasepp1.solid.community/profile/.acl";
 const acl = new aclClient(resource);
+const podClient = new PodClient();
 
 describe("adding an Agent", () => {
   before("Setting up auth...", async function () {
@@ -89,14 +89,45 @@ describe("adding an Agent", () => {
         });
       });
     });
+
+    it("adds an agent to a new acl file", async () => {
+      const testFile = "https://lalasepp1.solid.community/test.txt";
+      await podClient.create(testFile, { contents: "lala" });
+      const newAcl = new aclClient(testFile);
+      const agentToAdd = {
+        name: "https://ludwig.owntech.de/profile/card#me",
+        type: "Agent",
+        access: ["http://www.w3.org/ns/auth/acl#Read"],
+      };
+      const agentsToMatch = [
+        {
+          name: "https://lalasepp1.solid.community/profile/card#me",
+          type: "Agent",
+          identifier:
+            "https://lalasepp1.solid.community/profile/.acl#ControlReadWrite",
+          access: [
+            "http://www.w3.org/ns/auth/acl#Control",
+            "http://www.w3.org/ns/auth/acl#Read",
+            "http://www.w3.org/ns/auth/acl#Write",
+          ],
+        },
+        agentToAdd,
+      ];
+      await newAcl.addAgent(agentToAdd);
+      const agents = await newAcl.readAccessControl();
+      expect(newAcl.defaultAclResource).to.be.undefined;
+      expect(agents[0].identifier.includes(newAcl.aclResource)).to.be.true;
+      expect(agents).to.deep.equal(agentsToMatch);
+    });
   });
 
-  after("Cleaning up", () => {
+  after("Cleaning up", async () => {
+    await podClient.delete("https://lalasepp1.solid.community/test.txt");
     const addedAgent = {
       name: "https://ludwig.owntech.de/profile/card#me",
       type: "Agent",
       access: ["http://www.w3.org/ns/auth/acl#Read"],
     };
-    return acl.deleteAgent(addedAgent);
+    return await acl.deleteAgent(addedAgent);
   });
 });
